bc20800c01b4de118f84e7dbb99ab29d90e03d71
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-26 00:46:10 +0530

    1

--- src/main/java/com/driver/AppRankingComparator.java
+++ src/main/java/com/driver/AppRankingComparator.java
@@ -1,9 +1,15 @@
 package com.driver;
 
-public class AppRankingComparator {
-	 public int compare(NetBankingApp app1, NetBankingApp app2) {
-	        // your code goes here
+import java.util.Comparator;
 
-	        return Integer.compare(app2.getNumOfFeatures(), app1.getNumOfFeatures());
-	    }
+public class AppRankingComparator implements Comparator<NetBankingApp> {
+	public int compare(NetBankingApp app1, NetBankingApp app2) {
+		int compareSecurity = Integer.compare(app2.getSecurityRating(), app1.getSecurityRating());
+		if (compareSecurity != 0) return compareSecurity;
+
+		int compareFriendliness = Integer.compare(app2.getUserFriendliness(), app1.getUserFriendliness());
+		if (compareFriendliness != 0) return compareFriendliness;
+
+		return Integer.compare(app2.getNumOfFeatures(), app1.getNumOfFeatures());
+	}
 }
--- src/main/java/com/driver/NetBankingApp.java
+++ src/main/java/com/driver/NetBankingApp.java
@@ -1,32 +1,31 @@
 package com.driver;
 
 public class NetBankingApp {
-	private String appName;
+    private String appName;
     private int securityRating;  // out of 10
     private int userFriendliness; // out of 10
     private int numOfFeatures;
 
     public NetBankingApp(String appName, int securityRating, int userFriendliness, int numOfFeatures) {
-      // your code goes here
+        this.appName = appName;
+        this.securityRating = securityRating;
+        this.userFriendliness = userFriendliness;
+        this.numOfFeatures = numOfFeatures;
     }
 
     public String getAppName() {
-    	// your code goes here
         return appName;
     }
 
     public int getSecurityRating() {
-    	// your code goes here
         return securityRating;
     }
 
     public int getUserFriendliness() {
-    	// your code goes here
         return userFriendliness;
     }
 
     public int getNumOfFeatures() {
-    	// your code goes here
         return numOfFeatures;
     }
 }
